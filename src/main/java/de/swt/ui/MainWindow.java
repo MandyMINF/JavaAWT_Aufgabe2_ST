ca44c29a40b3f20c798b93ca384048bd2b8bef37
Mandy <mandyroth1519@gmail.com>
2022-10-05 17:26:13 +0200

    Version 1.3

--- src/main/java/de/swt/ui/MainWindow.java
+++ src/main/java/de/swt/ui/MainWindow.java
@@ -13,7 +13,7 @@ public class MainWindow extends Frame {
 
         public MainWindow(){
             setSize(500,300);
-            setTitle("Metrik Version 1.2.2");
+            setTitle("Metrik Version 1.3");
 
             mymenu = new MyMenu(this);
             add(canvas);
